a335fcc91177901509faf7289999b6ba5ec372e4
kharaneAnand <anandkharane@gmail.com>
2025-06-15 17:58:26 +0530

    created the file structure and in setting the routes using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route , Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Favorite from './pages/Favorite'
+import MovieDetails from './pages/MovieDetails'
+import Movies from './pages/Movies'
+import SeatLayout from './pages/SeatLayout'
+import MyBooking from './pages/MyBooking'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/Movies' element = {<Movies/>} />
+        <Route path='/Movies/:id' element = {<MovieDetails/>} />
+        <Route path='/Movies/:id/:date' element = {<SeatLayout/>} />
+        <Route path='/my-Booking' element = {<MyBooking/>} />
+        <Route path='/Favorite' element = {<Favorite/>} />
+      </Routes>
+    </>
   )
 }
 
