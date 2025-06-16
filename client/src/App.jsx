3aa0938bef6546ec2d6218271370c780ca1f0e84
kharaneAnand <anandkharane@gmail.com>
2025-06-16 10:33:31 +0530

    SetUp completed tailwind insatll and file structure is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route , Routes } from 'react-router-dom'
+import { Route , Routes , useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Favorite from './pages/Favorite'
 import MovieDetails from './pages/MovieDetails'
 import Movies from './pages/Movies'
 import SeatLayout from './pages/SeatLayout'
 import MyBooking from './pages/MyBooking'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin') 
+
   return (
     <>
-      <Navbar/>
+      <Toaster/>
+      { !isAdminRoute && <Navbar/>}
       <Routes>
         <Route path='/' element = {<Home/>} />
         <Route path='/Movies' element = {<Movies/>} />
@@ -20,6 +26,7 @@ const App = () => {
         <Route path='/my-Booking' element = {<MyBooking/>} />
         <Route path='/Favorite' element = {<Favorite/>} />
       </Routes>
+       { !isAdminRoute && <Footer/>}
     </>
   )
 }
