191b14898079eeabd6ce342d5829e39b44190a64
kharaneAnand <anandkharane@gmail.com>
2025-06-18 13:40:03 +0530

    completed the TrailerSceion pn the home page

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import timeFormat from '../lib/timeFormat';
 
 
 const MovieCard = ({movie}) => {
@@ -11,7 +12,7 @@ const MovieCard = ({movie}) => {
         <img onClick={()=> {navigate (`/movies/${movie._id}`) ; scrollTo(0,0)}} src={movie.backdrop_path} alt="Movie card img" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer' />
         <p className='font-semibold mt-2 truncate'>{movie.title}</p>
         <p className='text-sm text-gray-400 mt-2'>
-            {new Date(movie.release_date).getFullYear()} . {movie.genres.slice(0,2).map(genre =>genre.name).join("|")} . {movie.runtime}
+            {new Date(movie.release_date).getFullYear()} . {movie.genres.slice(0,2).map(genre =>genre.name).join("|")} . {timeFormat(movie.runtime)}
         </p>    
 
         <div className='flex items-center justify-between mt-4 pb-3'>
