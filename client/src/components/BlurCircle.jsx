83f9bc762ec07e7e6799e2c6f7d47744d4e2a952
kharaneAnand <anandkharane@gmail.com>
2025-06-20 12:29:51 +0530

    complete and created the Movies and Favorites page

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const BlurCircle = ( {top = "auto" , left="auto" , right ="auto" , bottom = "auto"} ) => {
+const BlurCircle = ({
+  top = "auto",
+  left = "auto",
+  right = "auto",
+  bottom = "auto",
+}) => {
   return (
-    <div className='absolute -z-50 h-58 w-58 aspect-square rounded-full bg-primary/30 blur-3xl ' style={{top:top , left:left , right:right , bottom:bottom}} ></div>
-  )
-}
+    <div
+      className="absolute -z-50 aspect-square w-56 rounded-full bg-primary/30 blur-3xl"
+      style={{ top, left, right, bottom }}
+    ></div>
+  );
+};
 
-export default BlurCircle
+export default BlurCircle;
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "../components/MovieCard.jsx";
+import BlurCircle from "../components/BlurCircle.jsx";
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
-}
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      <BlurCircle top="150px" right="0px" />
+      <BlurCircle top="50px" right="50px" />
+      <h1 className="text-lg font-medium my-4">Now Showing </h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div>
+      <h1>No Movies Available</h1>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
