4c4b370118b209236234beb00535e8a78d840e71
kharaneAnand <anandkharane@gmail.com>
2025-06-21 14:10:05 +0530

    completd the my-booking page in the clients website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Route , Routes , useLocation } from 'react-router-dom'
-import Home from './pages/Home'
-import Favorite from './pages/Favorite'
-import MovieDetails from './pages/MovieDetails'
-import Movies from './pages/Movies'
-import SeatLayout from './pages/SeatLayout'
-import MyBooking from './pages/MyBooking'
-import { Toaster } from 'react-hot-toast'
-import Footer from './components/Footer'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorite from "./pages/Favorite";
+import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
+import SeatLayout from "./pages/SeatLayout";
+import MyBooking from "./pages/MyBooking";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
-
-  const isAdminRoute = useLocation().pathname.startsWith('/admin') 
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
 
   return (
     <>
-      <Toaster/>
-      { !isAdminRoute && <Navbar/>}
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element = {<Home/>} />
-        <Route path='/Movies' element = {<Movies/>} />
-        <Route path='/Movies/:id' element = {<MovieDetails/>} />
-        <Route path='/Movies/:id/:date' element = {<SeatLayout/>} />
-        <Route path='/my-Booking' element = {<MyBooking/>} />
-        <Route path='/Favorite' element = {<Favorite/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Movies" element={<Movies />} />
+        <Route path="/Movies/:id" element={<MovieDetails />} />
+        <Route path="/Movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBooking />} />
+        <Route path="/Favorite" element={<Favorite />} />
       </Routes>
-       { !isAdminRoute && <Footer/>}
+      {!isAdminRoute && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
